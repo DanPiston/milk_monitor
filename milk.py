fb1c20aacafbfd30378a4d575228503541ae3198
DanPiston <danpiston@gmail.com>
2016-06-20 21:01:26 -0400

    Now writes the day to a text file

--- milk.py
+++ milk.py
@@ -1,18 +1,20 @@
-milk_entry = {'Time of Day': '',
-              'Starting Oz': '',
-              'Ending Oz': '',
-              'Total Oz': ''}
+#Writes and/or creates the file
+def create_file(x):
+    f_name = input = raw_input('What is the day?: ')
+    f_write = open(f_name + '.txt', 'a')
+    f_write.write('\n' + x)
+    f_write.close()
 
-
+#Gathers the days info
 def get_milk():
-    
     time_of_day =input = raw_input("What Time?: ")
     start_oz = input = raw_input('Starting Oz?: ')
-    end_oz =  input = raw_input('Finishing Oz?: ')
+    end_oz =  input = raw_input('Any ounces left?: ')
     total_oz = int(start_oz) - int(end_oz)
-    print('\nAt ' + time_of_day + '\nWe started with ' + str(start_oz) + '\nThen we finished with ' + str(end_oz) + '\nHe Drank ' + str(total_oz))
+    statement=  '\nAt ' + time_of_day + '\nWe started with ' + str(start_oz) + 'Ounces' + '\nThen we finished with ' + str(end_oz) + 'Ounces'+ '\nHe Drank ' + str(total_oz) + ' Ounces' 
+    return(statement)
 
 def main():
-    get_milk()
+    create_file(get_milk())
 
 main()
---
+++ test_write.py
@@ -0,0 +1,6 @@
+#Testing writing to a file
+f_name = input = raw_input('What is the day?: ')
+writter = open(f_name+'.txt', 'w')
+writter.write('test')
+
+
